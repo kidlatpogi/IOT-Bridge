e7a58067551e11300afb9719537435b57267e0b1
User <user@example.com>
2025-12-28 16:45:05 +0800

    Removed Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+  const [devices, setDevices] = useState([]);
+
+  const addDevice = () => {
+    const deviceName = prompt("Enter device name:");
+    if (deviceName) {
+      setDevices(prev => [...prev, { name: deviceName }]);
+      console.log("Device added:", deviceName);
+    }
+  }
 
   return (
     <>
-      <h1>I.O.T Bridge</h1>
+      <header><h1>I.O.T Bridge</h1></header>
+
+      <main>
+        <button onClick={addDevice}>Add Device</button>
+        <ul>
+          {devices.map((d, i) => <li key={i}>{d.name}</li>)}
+        </ul>
+      </main>
     </>
   )
 }
